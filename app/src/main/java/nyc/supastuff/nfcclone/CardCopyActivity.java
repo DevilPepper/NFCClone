76f0d5ffc4fe547d26794c8692a7d393610e3069
SupaStuff <StuffQilla@gmail.com>
2017-07-25 22:19:27 -0400

    Made some other activities

--- app/src/main/java/nyc/supastuff/nfcclone/CardCopyActivity.java
+++ app/src/main/java/nyc/supastuff/nfcclone/CardCopyActivity.java
@@ -1,8 +1,20 @@
 package nyc.supastuff.nfcclone;
 
-/**
- * Created by Stuff on 7/23/2017.
- */
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-class CardCopyActivity {
+public class CardCopyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_copy);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
 }
--- app/src/main/java/nyc/supastuff/nfcclone/CardEmitterActivity.java
+++ app/src/main/java/nyc/supastuff/nfcclone/CardEmitterActivity.java
@@ -1,6 +1,5 @@
 package nyc.supastuff.nfcclone;
 
-import android.content.Context;
 import android.content.Intent;
 import android.nfc.NdefMessage;
 import android.os.Bundle;
@@ -18,52 +17,6 @@ public class CardEmitterActivity extends AppCompatActivity {
     private NdefMessage cardData;
     private int cardID;
 
-    private Context that;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_delete:
-                    //TODO Delete this card from DB. Also prevent this activity from returning.
-                    Intent deleteCard = new Intent(that, CardListActivity.class);
-                    that.startActivity(deleteCard);
-                    return true;
-                case R.id.navigation_camera:
-                    Intent updateCardImage = new Intent(that, CameraActivity.class);
-                    that.startActivity(updateCardImage);
-                    displayImage();
-                    return true;
-                case R.id.navigation_scan:
-                    Intent updateCardDetails = new Intent(that, CardCopyActivity.class);
-                    that.startActivity(updateCardDetails);
-                    emitData();
-                    return true;
-            }
-            return false;
-        }
-
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        that = this.getApplicationContext();
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_card_emitter);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        mTextMessage = (TextView)  findViewById(R.id.textView);
-        mImage       = (ImageView) findViewById(R.id.imageView);
-
-        //TODO set cardID somehow;
-        displayImage();
-        emitData();
-    }
 
     private void emitData() {
         //TODO Get card data from DB and pass it to emitter
@@ -74,5 +27,57 @@ public class CardEmitterActivity extends AppCompatActivity {
         //TODO Get image from DB. If no image, leave default.
         mImage.setImageResource(R.drawable.ic_dashboard_black_24dp);
         mTextMessage.bringToFront();
+        mTextMessage.invalidate();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_delete:
+                    //TODO Delete this card from DB. Also prevent this activity from returning.
+                    //Intent deleteCard = new Intent(getApplicationContext(), CardListActivity.class);
+                    //startActivity(deleteCard);
+                    finish();
+                    return true;
+                case R.id.navigation_camera:
+                    Intent updateCardImage = new Intent(getApplicationContext(), CameraActivity.class);
+                    startActivity(updateCardImage);
+                    displayImage();
+                    return true;
+                case R.id.navigation_scan:
+                    Intent updateCardDetails = new Intent(getApplicationContext(), CardCopyActivity.class);
+                    startActivity(updateCardDetails);
+                    emitData();
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_emitter);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mTextMessage = (TextView)  findViewById(R.id.textView);
+        mImage       = (ImageView) findViewById(R.id.imageView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //TODO set cardID somehow;
+        displayImage();
+        emitData();
+
     }
 }
